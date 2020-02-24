59ae93084acd1a8d1475850d6722e322d5d4a77b
Amaterasus <skyetay03@googlemail.com>
2020-02-24 09:33:26 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+   
+    attr_accessor :name
+   
+    def initialize(name)
+      @name = name
+    end
+  end
